efbb9fcfd41c264682027457da4f507eb5bdfdf0
Siva Kumar <sivakumar9399959803@gmail.com>
2023-07-24 14:58:12 +0530

    roboshop-terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,24 @@
 module "vpc" {
-  source      = "git::https://github.com/siva-devops73/tf-module-vpc.git"
+  source         = "git::https://github.com/siva-devops73/tf-module-vpc.git"
 
-  for_each = var.vpc
-  cidr_block = each.value["cidr_block"]
-  subnets = each.value["subnets"]
+  for_each       = var.vpc
+  cidr_block     = each.value["cidr_block"]
+  subnets        = each.value["subnets"]
 
-  env = var.env
-  tags = var.tags
+  env            = var.env
+  tags           = var.tags
   default_vpc_id = var.default_vpc_id
+}
+
+
+#module "vpc" {
+#  source      = "git::https://github.com/siva-devops73/tf-module-app.git"
+
+#  env            = var.env
+#  tags           = var.tags
+ # components     = var.components
+ # subnet_id      =
+
+output "subnets_ids" {
+  value = module.vpc
 }
