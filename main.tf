f2d305a99fd6b1c20ce5fb168dd0e51100908fb3
Siva Kumar <sivakumar9399959803@gmail.com>
2023-08-23 07:49:22 +0530

    roboshop-terraform

--- main.tf
+++ main.tf
@@ -12,90 +12,90 @@ module "vpc" {
 }
 
 
-#module "rabbitmq" {
-  #source          = "git::https://github.com/siva-devops73/tf-module-rabbitmq.git"
+module "rabbitmq" {
+  source          = "git::https://github.com/siva-devops73/tf-module-rabbitmq.git"
 
-  #for_each        = var.rabbitmq
-  #component       = each.value["component"]
-  #instance_type   = each.value["instance_type"]
+  for_each        = var.rabbitmq
+  component       = each.value["component"]
+  instance_type   = each.value["instance_type"]
 
-  #sg_subnet_cidr  = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
-  #vpc_id          = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  #subnet_id       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)[0]
+  sg_subnet_cidr  = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  vpc_id          = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  subnet_id       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)[0]
 
-  #env             = var.env
-  #tags            = var.tags
-  #allow_ssh_cidr  = var.allow_ssh_cidr
-  #zone_id         = var.zone_id
-  #kms_key_id      = var.kms_key_arn
+  env             = var.env
+  tags            = var.tags
+  allow_ssh_cidr  = var.allow_ssh_cidr
+  zone_id         = var.zone_id
+  kms_key_id      = var.kms_key_arn
 
-#}
-
-
-#module "rds" {
-  #source = "git::https://github.com/siva-devops73/tf-module-rds.git"
-
-  #for_each          = var.rds
-  #component         = each.value["component"]
-  #engine            = each.value["engine"]
-  #engine_version    = each.value["engine_version"]
-  #database_name     = each.value["database_name"]
-  #instance_count    = each.value["instance_count"]
-  #instance_class    = each.value["instance_class"]
-
-  #subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-  #vpc_id            = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  #sg_subnet_cidr    = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
-
-  #kms_key_arn       = var.kms_key_arn
-  #env               = var.env
-  #tags              = var.tags
-
-#}
+}
 
 
-#module "documentdb" {
-  #source = "git::https://github.com/siva-devops73/tf-module-documentdb.git"
+module "rds" {
+  source = "git::https://github.com/siva-devops73/tf-module-rds.git"
 
-  #for_each          = var.documentdb
-  #component         = each.value["component"]
-  #engine            = each.value["engine"]
-  #engine_version    = each.value["engine_version"]
-  #instance_count    = each.value["instance_count"]
-  #instance_class    = each.value["instance_class"]
+  for_each          = var.rds
+  component         = each.value["component"]
+  engine            = each.value["engine"]
+  engine_version    = each.value["engine_version"]
+  database_name     = each.value["database_name"]
+  instance_count    = each.value["instance_count"]
+  instance_class    = each.value["instance_class"]
 
-  #subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-  #vpc_id            = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  #sg_subnet_cidr    = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+  vpc_id            = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  sg_subnet_cidr    = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
 
-  #kms_key_arn       = var.kms_key_arn
-  #env               = var.env
-  #tags              = var.tags
+  kms_key_arn       = var.kms_key_arn
+  env               = var.env
+  tags              = var.tags
 
-#}
-
-
-#module "elasticache" {
-  #source = "git::https://github.com/siva-devops73/tf-module-elasticache.git"
-
-  #for_each               = var.elasticache
-  #component              = each.value["component"]
-  #engine                 = each.value["engine"]
-  #engine_version         = each.value["engine_version"]
-  #node_type              = each.value["node_type"]
-  #num_node_groups        = each.value["num_node_groups"]
-  #replicas_per_node_group = each.value["replicas_per_node_group"]
+}
 
 
-  #subnet_ids              = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-  #vpc_id                  = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  #sg_subnet_cidr          = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+module "documentdb" {
+  source = "git::https://github.com/siva-devops73/tf-module-documentdb.git"
 
-  #kms_key_arn             = var.kms_key_arn
-  #env                     = var.env
-  #tags                    = var.tags
+  for_each          = var.documentdb
+  component         = each.value["component"]
+  engine            = each.value["engine"]
+  engine_version    = each.value["engine_version"]
+  instance_count    = each.value["instance_count"]
+  instance_class    = each.value["instance_class"]
 
-#}
+  subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+  vpc_id            = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  sg_subnet_cidr    = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+
+  kms_key_arn       = var.kms_key_arn
+  env               = var.env
+  tags              = var.tags
+
+}
+
+
+module "elasticache" {
+  source = "git::https://github.com/siva-devops73/tf-module-elasticache.git"
+
+  for_each               = var.elasticache
+  component              = each.value["component"]
+  engine                 = each.value["engine"]
+  engine_version         = each.value["engine_version"]
+  node_type              = each.value["node_type"]
+  num_node_groups        = each.value["num_node_groups"]
+  replicas_per_node_group = each.value["replicas_per_node_group"]
+
+
+  subnet_ids              = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+  vpc_id                  = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  sg_subnet_cidr          = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+
+  kms_key_arn             = var.kms_key_arn
+  env                     = var.env
+  tags                    = var.tags
+
+}
 
 
 module "alb" {
